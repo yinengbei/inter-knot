124fea50d49e31655d2e0684e788ad910e17cd93
yinengbei <yinengbei@users.noreply.github.com>
2026-02-11 09:35:12 +0000

    deploy: 2620c9ab32b651b9e9630749e55fdda307143090

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "assets/assets/images/zzz.webp": "9013567fa7f4f8bc8872978615756767",
 "assets/assets/images/discussion-page-bg-point.webp": "54d2fc199763ee5e5b519d84d2472bbc",
 "assets/assets/fonts/zh-cn.ttf": "445056b6c3381dc87be111520518432e",
-"assets/fonts/MaterialIcons-Regular.otf": "4e502e20337df2b57e0777533df34b4a",
+"assets/fonts/MaterialIcons-Regular.otf": "c5856c777bc72b9d76afc482802b4c30",
 "assets/NOTICES": "3d2fc615d3c771210a5c116d2a6dcfd9",
 "assets/packages/material_design_icons_flutter/lib/fonts/materialdesignicons-webfont.ttf": "d10ac4ee5ebe8c8fff90505150ba2a76",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "d7d83bd9ee909f8a9b348f56ca7b68c6",
@@ -46,7 +46,7 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "10fe76aabff774f2652bf8125aabeb64",
 "flutter_bootstrap.js": "1678d6fdce0f68da4a988a0228338e23",
 "version.json": "8dd3884697f498447b0263792adfd8b7",
-"main.dart.js": "aa38b594a8e2fd60771e5ce351f59ad6"};
+"main.dart.js": "06107e487b7a750b064b99d9e56811ce"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
